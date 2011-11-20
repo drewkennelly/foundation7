29ce4d4d7d7ea486a463fad7ade3d048b58643a1
Drew Kennelly <drewkennelly@gmail.com>
2011-11-20 21:12:03 +1100

    Adding class for buttons

--- template.php
+++ template.php
@@ -41,3 +41,17 @@
   
     return $output . $extra;
   }
+  
+  function theme_button($variables) {
+    $element = $variables['element'];
+    $element['#attributes']['type'] = 'submit';
+    element_set_attributes($element, array('id', 'name', 'value'));
+  
+    $element['#attributes']['class'] = array('form-' . $element['#button_type'], 'blue', 'nice', 'button', 'radius');
+
+    if (!empty($element['#attributes']['disabled'])) {
+      $element['#attributes']['class'][] = 'form-button-disabled';
+    }
+  
+    return '<input' . drupal_attributes($element['#attributes']) . ' />';
+  }
