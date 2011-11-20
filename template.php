0935331e39622eeee1a9324d42f0ac3cf69b7ecc
Drew Kennelly <drewkennelly@gmail.com>
2011-11-20 20:14:50 +1100

    Added nice class to forms

---
+++ template.php
@@ -0,0 +1,18 @@
+<?php
+
+  function foundation_form($variables) {
+    $element = $variables['element'];
+    if (isset($element['#action'])) {
+      $element['#attributes']['action'] = drupal_strip_dangerous_protocols($element['#action']);
+    }
+    element_set_attributes($element, array('method', 'id'));
+    if (empty($element['#attributes']['accept-charset'])) {
+      $element['#attributes']['accept-charset'] = "UTF-8";
+    }
+    
+    //add nice to form class
+    $element['#attributes']['class'] .= ' nice';
+    
+    // Anonymous DIV to satisfy XHTML compliance.
+    return '<form' . drupal_attributes($element['#attributes']) . '><div>' . $element['#children'] . '</div></form>';
+  }
