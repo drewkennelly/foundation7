e4e42531d1922541df1f3f96614d06349c02c8e8
Drew Kennelly <drewkennelly@gmail.com>
2011-11-20 21:02:58 +1100

    Added extra js files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
-  $('form').addClass('nice');
-		
+	/* Use this js doc for all application specific JS */	
+	
+	/* PLACEHOLDER FOR FORMS ------------- */
+	/* Remove this and jquery.placeholder.min.js if you don't need :) */
+	
+	$('input, textarea').placeholder();
+	
+	
+	/* DISABLED BUTTONS ------------- */
+	/* Gives elements with a class of 'disabled' a return: false; */
+	
+	
 });
