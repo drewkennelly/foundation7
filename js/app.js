bf46ea4c5217f33d120b6d3c0c4b3b6b76790875
Drew Kennelly <drewkennelly@gmail.com>
2011-11-25 14:17:35 +1100

    Updated js to new versions from framework

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	/* Use this js doc for all application specific JS */	
+	/* Use this js doc for all application specific JS */
+
+	/* TABS --------------------------------- */
+	/* Remove if you don't need :) */
+	
+	function activateTab($tab) {
+	  var $activeTab = $tab.closest('dl').find('a.active'),
+	      contentLocation = $tab.attr("href") + 'Tab';
+	      
+	  //Make Tab Active
+	  $activeTab.removeClass('active');
+	  $tab.addClass('active');
+	  
+    //Show Tab Content
+		$(contentLocation).closest('.tabs-content').find('li').hide();
+		$(contentLocation).show();
+	}
+	
+	$('dl.tabs').each(function () {
+		//Get all tabs
+		var tabs = $(this).children('dd').children('a');
+		tabs.click(function (e) {
+		  activateTab($(this));
+		});
+	});
+	
+	if (window.location.hash) {
+    activateTab($('a[href="' + window.location.hash + '"]'));
+  }
+	
 	
 	/* PLACEHOLDER FOR FORMS ------------- */
 	/* Remove this and jquery.placeholder.min.js if you don't need :) */
