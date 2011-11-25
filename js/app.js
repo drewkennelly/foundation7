a9af1ec17e65ac6adf825d6da0d96227479ea44b
Drew Kennelly <drewkennelly@gmail.com>
2011-11-25 14:51:39 +1100

    Added code to close message

--- js/app.js
+++ js/app.js
@@ -1,44 +1,12 @@
 $(document).ready(function () {
 
 	/* Use this js doc for all application specific JS */
-
-	/* TABS --------------------------------- */
-	/* Remove if you don't need :) */
 	
-	function activateTab($tab) {
-	  var $activeTab = $tab.closest('dl').find('a.active'),
-	      contentLocation = $tab.attr("href") + 'Tab';
-	      
-	  //Make Tab Active
-	  $activeTab.removeClass('active');
-	  $tab.addClass('active');
-	  
-    //Show Tab Content
-		$(contentLocation).closest('.tabs-content').find('li').hide();
-		$(contentLocation).show();
-	}
-	
-	$('dl.tabs').each(function () {
-		//Get all tabs
-		var tabs = $(this).children('dd').children('a');
-		tabs.click(function (e) {
-		  activateTab($(this));
-		});
+	//close messages on click
+	$(".close").click(function(){
+		$(this).parent().remove();
+		return false;
 	});
-	
-	if (window.location.hash) {
-    activateTab($('a[href="' + window.location.hash + '"]'));
-  }
-	
-	
-	/* PLACEHOLDER FOR FORMS ------------- */
-	/* Remove this and jquery.placeholder.min.js if you don't need :) */
-	
-	$('input, textarea').placeholder();
-	
-	
-	/* DISABLED BUTTONS ------------- */
-	/* Gives elements with a class of 'disabled' a return: false; */
 	
 	
 });
