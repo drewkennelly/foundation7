0722d18a2e4386ebf7f005cf5f4e6ad2ec606436
Drew Kennelly <drewkennelly@gmail.com>
2011-11-25 14:42:49 +1100

    Theming messages

--- template.php
+++ template.php
@@ -69,3 +69,41 @@
   
     return '<input' . drupal_attributes($element['#attributes']) . ' />';
   }
+	
+	
+	function foundation_status_messages($variables) {
+		$display = $variables['display'];
+		$output = '';
+	
+		$status_heading = array(
+			'status' => t('Status message'), 
+			'error' => t('Error message'), 
+			'warning' => t('Warning message'),
+		);
+		foreach (drupal_get_messages($display) as $type => $messages) {
+			
+			//change 'status' to 'success'
+			if ( $type == 'status' )
+				$type = 'success';
+			else
+				$type = $type;
+			
+			$output .= "<div class=\"alert-box $type\">\n";
+			if (!empty($status_heading[$type])) {
+				$output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+			}
+			if (count($messages) > 1) {
+				$output .= " <ul>\n";
+				foreach ($messages as $message) {
+					$output .= '  <li>' . $message . "</li>\n";
+				}
+				$output .= " </ul>\n";
+			}
+			else {
+				$output .= $messages[0];
+			}
+			$output .= "<a href="" class='close'>&times;</a>
+			</div>\n";
+		}
+		return $output;
+	}
