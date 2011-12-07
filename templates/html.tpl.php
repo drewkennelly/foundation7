40e015fe5cf48e237221689bdbe07ac7fff17c03
Drew Kennelly <drewkennelly@gmail.com>
2011-12-08 10:39:50 +1100

    Removed charset from template as drupal generates it

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html class="<?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 <head profile="<?php print $grddl_profile; ?>">
-  <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width" />
   <?php echo $head; ?>
   <title><?php echo $head_title; ?></title>
