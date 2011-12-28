da3b45a444fa75f1cc628e6fc0eea5b5a2060837
Drew Kennelly <drewkennelly@gmail.com>
2011-12-28 15:40:52 +1100

    Structural changes

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,44 +1,46 @@
 <div class="container">
+
   <div class="row site-header">
     <div class="twelve columns">
       <h1><a href="<?php $front; ?>"><?php echo $site_name; ?></a></h1>
       <h4 class="subheader"><?php echo $site_slogan; ?></h4>
-      <hr />
     </div>
   </div>
+  
   <div class="row">
-    <div class="three columns sidebar">
-			<?php print render($page['left']); ?>
+    <div class="twelve columns">
+			<?php echo render($page['nav']); ?>
     </div>
-    <div class="nine columns">
+  </div>
+  
+  <div class="row">
+    <div class="twelve columns main">
       <?php echo $messages; ?>
+      
 			<?php if ($tabs): ?>
       	<div class="tabs"><?php print render($tabs); ?></div>
       <?php endif; ?>
+      
       <?php print render($title_prefix); ?>
       <h2><?php echo $title; ?></h2>
       <?php print render($title_suffix); ?>
+      
       <?php if ($action_links): ?>
       	<ul class="action-links">
       		<?php print render($action_links); ?>
       	</ul>
       <?php endif; ?>
+      
       <?php echo render($page['content']); ?>
     </div>
   </div>
-  <div class="row">
-    <div class="twelve columns">
-    	<hr />
+  
+  <?php if ( $page['footer'] ): ?>
+    <div class="row">
+      <div class="twelve columns">
+  			<?php echo render($page['footer_left']); ?>
+      </div>
     </div>
-  </div>
-  <?php if ( $page['footer_left'] || $page['footer_right'] ): ?>
-  <div class="row">
-    <div class="eleven columns">
-			<?php echo render($page['footer_left']); ?>
-    </div>
-    <div class="one columns">
-			<?php echo render($page['footer_right']); ?>
-		</div>
-  </div>
   <?php endif; ?>
+  
 </div>
