7ac8ea71caae701cfcbb721a8acff53a6479a220
Drew Kennelly <drewkennelly@gmail.com>
2012-01-13 11:23:41 +1100

    Adding block.tpl.php

---
+++ templates/block.tpl.php
@@ -0,0 +1,13 @@
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+	
+	<?php print render($title_prefix); ?>
+  <?php if ($block->subject): ?>
+  	<h4<?php print $title_attributes; ?>><?php print $block->subject ?></h4>
+  <?php endif;?>
+  <?php print render($title_suffix); ?>
+  
+  <div class="content"<?php print $content_attributes; ?>>
+		<?php print $content ?>
+  </div>
+  
+</div>
