d8164b87e62dbfcd1c1c33ba13a890c33a27d766
Drew Kennelly <drewkennelly@gmail.com>
2012-01-17 14:33:30 +1100

    Styling h2

--- templates/block.tpl.php
+++ templates/block.tpl.php
@@ -2,7 +2,7 @@
 	
 	<?php print render($title_prefix); ?>
   <?php if ($block->subject): ?>
-  	<h4<?php print $title_attributes; ?>><?php print $block->subject ?></h4>
+  	<h5<?php print $title_attributes; ?>><?php print $block->subject ?></h5>
   <?php endif;?>
   <?php print render($title_suffix); ?>
 
