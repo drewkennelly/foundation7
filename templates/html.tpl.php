c0f22b3ea68eb70e2dd4f970ef046a1c1bfb1871
Drew Kennelly <drewkennelly@gmail.com>
2012-01-17 11:04:56 +1100

    ie and js fixes

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,32 +1,50 @@
 <!DOCTYPE html>
 
-<html class="<?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7 <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8 <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9 <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="<?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>> <!--<![endif]-->
 
-	<head profile="<?php print $grddl_profile; ?>">
-	  <meta charset="utf-8" />
-	  <meta name="viewport" content="width=device-width" />
-	  <?php echo $head; ?>
-		<!-- Head -->
+<head profile="<?php print $grddl_profile; ?>">
 	
-	  <title><?php echo $head_title; ?></title>
-	  <?php echo $styles; ?>
-		<!-- Styles -->
-	
-	  <?php echo $scripts; ?>
-		<!-- Scripts -->
-	
-	</head>
+  <meta charset="utf-8" />
 
-	<body <?php print $attributes;?>>
+  <meta name="viewport" content="width=device-width" />
+	<!-- Set the viewport width to device width for mobile -->
+
+  <?php echo $head; ?>
+	<!-- Head -->
+
+  <title><?php echo $head_title; ?></title>
+
+  <?php echo $styles; ?>
+	<!-- Styles -->
 	
-		<?php print $page_top; ?>
-		<!-- Page top -->
-	
-	  <?php print $page; ?>
-		<!-- Page -->
-	
-	  <?php print $page_bottom; ?>
-		<!-- Page bottom -->
-	
-	</body>
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="stylesheets/ie.css">
+	<![endif]-->
+
+	<!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+  <?php echo $scripts; ?>
+	<!-- Scripts -->
+
+</head>
+
+<body <?php print $attributes;?>>
+
+	<?php print $page_top; ?>
+	<!-- Page top -->
+
+  <?php print $page; ?>
+	<!-- Page -->
+
+  <?php print $page_bottom; ?>
+	<!-- Page bottom -->
+
+</body>
 </html>
