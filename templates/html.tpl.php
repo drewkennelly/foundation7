2e2af76cee374c7af2594a7b359f1a8d739e42d8
Drew Kennelly <drewkennelly@gmail.com>
2012-02-29 11:30:58 +1100

    css changes from Zurb. Changing html.tpl.php to keep in line with zurb index.html

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -4,10 +4,10 @@
 <!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7 <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>> <![endif]-->
 <!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8 <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>> <![endif]-->
 <!--[if IE 8]>    <html class="no-js lt-ie9 <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="<?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>> <!--<![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js <?php print $classes; ?>" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>> <!--<![endif]-->
 
 <head profile="<?php print $grddl_profile; ?>">
-	
+
   <meta charset="utf-8" />
 
   <meta name="viewport" content="width=device-width" />
@@ -24,6 +24,8 @@
 	<!--[if lt IE 9]>
 	 <link rel="stylesheet" href="<?php print $directory; ?>/css/ie.css">
 	<![endif]-->
+
+  <script src="<?php print $directory; ?>/js/modernizr.foundation.js"></script>
 
 	<!-- IE Fix for HTML5 Tags -->
 	<!--[if lt IE 9]>
