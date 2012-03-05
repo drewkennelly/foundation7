3619ef53ec9a0b996c92d05c12f794fcde175ce5
Drew Kennelly <drewkennelly@gmail.com>
2012-03-05 11:10:39 +1100

    Removing project and block templates and css. Updating forms, globasls and tooltips. Simplifying if statement in pgae.tpl.php

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,16 +1,14 @@
 <div class="container">
 
-  <div class="row site-header">
-		<?php if ( $page['header'] ): ?>
-    	<div class="twelve columns">
-	      <?php print render( $page['header']); ?>
-	    </div>
-		<?php else: ?>
-	    <div class="twelve columns">
-	      <h1><a href="<?php print $front_page; ?>"><?php print $site_name; ?></a></h1>
-	      <h4 class="subheader"><?php print $site_slogan; ?></h4>
-	    </div>
-		<?php endif; ?>
+  <div class="row">
+    <div class="twelve columns">
+      <?php if ( $page['header'] ): ?>
+        <?php print render( $page['header']); ?>
+      <?php else: ?>
+        <h1><a href="<?php print $front_page; ?>"><?php print $site_name; ?></a></h1>
+        <h4 class="subheader"><?php print $site_slogan; ?></h4>
+      <?php endif; ?>
+    </div>
   </div>
 	<!-- Header -->
 
@@ -23,7 +21,7 @@
 		</div>
 		<!-- Main Navigation -->
 	<?php endif; ?>
-  
+
   <div class="row">
   	<div class="twelve columns">
     	<hr />
@@ -31,33 +29,33 @@
   </div>
 
   <div class="row">
-		
+
 		<?php if ( $page['left'] ): ?>
 	    <div class="three columns">
 	      <?php print render($page['left']); ?>
 	    </div>
 			<!-- Left sidebar -->
 	  <?php endif; ?>
-	
-	
+
+
     <div class="<?php print $main_columns; ?> columns">
-	
+
       <?php print $messages; ?>
-      
+
 			<?php if ($tabs): ?>
       	<div class="tabs"><?php print render($tabs); ?></div>
       <?php endif; ?>
-      
+
       <?php print render($title_prefix); ?>
       <h2><?php print $title; ?></h2>
       <?php print render($title_suffix); ?>
-      
+
       <?php if ($action_links): ?>
       	<ul class="action-links">
       		<?php print render($action_links); ?>
       	</ul>
       <?php endif; ?>
-      
+
       <?php print render($page['content']); ?>
     </div>
     <!-- Main content area -->
@@ -70,14 +68,14 @@
     <?php endif; ?>
 
   </div>
-  
+
   <?php if ( $page['footer'] ): ?>
     <div class="row">
       <div class="twelve columns">
         <hr />
       </div>
     </div>
-  
+
     <div class="row">
       <div class="twelve columns">
   			<?php print render($page['footer']); ?>
@@ -85,6 +83,6 @@
     </div>
 		<!-- Footer -->
   <?php endif; ?>
-  
+
 </div>
 <!-- Container -->
