b6585b8a407dd3f3235366952ed6b0c4b84f8527
Drew Kennelly <drewkennelly@gmail.com>
2012-09-24 17:56:38 +1000

    adding foundation theme to buttons

--- template.php
+++ template.php
@@ -1,8 +1,8 @@
 <?php
 
-	/*
-	Preprocess function to determine number of columns for main content area
-	*/
+	/**
+	*	Preprocess function to determine number of columns for main content area
+	**/
 	function foundation_preprocess_page(&$variables) {
 		if ( !empty($variables['page']['left']) && !empty($variables['page']['right']) )
 			$variables['main_columns'] = 'six';
@@ -10,4 +10,24 @@
 			$variables['main_columns'] = 'nine';
 		else if ( empty($variables['page']['left']) && empty($variables['page']['right']) )
 			$variables['main_columns'] = 'twelve';
+	}
+
+
+	/**
+	* Overwrite theme_button()
+	*/
+	function foundation_button($element) {
+
+	// Add some extra conditions to make sure we're only adding the classto the right submit button
+	if ($element['#id'] == 'edit-submit') {
+		// Now add our custom class
+		if (isset($element['#attributes']['class'])) {
+			$element['#attributes']['class'] .= ' button';
+		}
+		else {
+			$element['#attributes']['class'] = 'button';
+		}
+	}
+
+	return theme_button($element);
 	}
