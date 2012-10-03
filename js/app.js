254a94d0d3bc971cd0b320767a3ac965b9f27edb
Drew Kennelly <drewkennelly@gmail.com>
2012-10-03 12:22:04 +1000

    removing query as it is included in foundation.min.js

--- js/app.js
+++ js/app.js
@@ -4,17 +4,31 @@
   var $doc = $(document),
       Modernizr = window.Modernizr;
 
+  
   $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-  $.fn.foundationButtons          ? $doc.foundationButtons() : null;
   $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+  $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+  $('input, textarea').placeholder();
+  
+  
+  $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+  
+  
   $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+  
+  
   $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+  
   $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
   $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-  $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
-  $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-
-  $('input, textarea').placeholder();
+  
+    
+    $.fn.foundationTabs             ? $doc.foundationTabs() : null;
+    
+  
+  
+    $("#featured").orbit();
+  
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
   // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
