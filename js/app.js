6338a7678b012fb0bbec480d39fe732d3acedf66
Drew Kennelly <drewkennelly@gmail.com>
2012-11-05 11:59:12 +1100

    Updating to foundation 3.2

--- js/app.js
+++ js/app.js
@@ -4,31 +4,21 @@
   var $doc = $(document),
       Modernizr = window.Modernizr;
 
-  
-  $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-  $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-  $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-  $('input, textarea').placeholder();
-  
-  
-  $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-  
-  
-  $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-  
-  
-  $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-  
-  $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-  $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-  
-    
-    $.fn.foundationTabs             ? $doc.foundationTabs() : null;
-    
-  
-  
-    $("#featured").orbit();
-  
+  $(document).ready(function() {
+    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
+    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
+    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
+    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
+
+    $('input, textarea').placeholder();
+  });
 
   // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
   // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
@@ -36,8 +26,8 @@
   // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
   // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
 
-  // Hide address bar on mobile devices
-  if (Modernizr.touch) {
+  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+  if (Modernizr.touch && !window.location.hash) {
     $(window).load(function () {
       setTimeout(function () {
         window.scrollTo(0, 1);
