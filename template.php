db034e61755b9dfdc462986b5bb628303b870fb8
Drew Kennelly <drewkennelly@gmail.com>
2013-02-08 19:51:40 +1100

    updating path for js

--- template.php
+++ template.php
@@ -44,11 +44,11 @@
 	function foundation_breadcrumb($variables) {
 	  $breadcrumb = $variables['breadcrumb'];
 		if (!empty($breadcrumb)) {
-			$crumbs = '<ul class="breadcrumbs">';
+			$output = '<ul class="breadcrumbs">';
 			foreach($breadcrumb as $value) {
 				$crumbs .= '<li>'.$value.'</li>';
 			}
 			$crumbs .= '</ul>';
+			return $crumbs;
 		}
-		return $crumbs;
 	}
