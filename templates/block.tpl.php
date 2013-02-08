be0b8447530b12935fdc7a4558ed1f1574b229f8
Drew Kennelly <drewkennelly@gmail.com>
2013-02-08 20:46:09 +1100

    Stripping block markup

--- templates/block.tpl.php
+++ templates/block.tpl.php
@@ -1,13 +1,7 @@
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<?php print render($title_prefix); ?>
+<?php if ($block->subject): ?>
+	<h4<?php print $title_attributes; ?>><?php print $block->subject ?></h4>
+<?php endif;?>
+<?php print render($title_suffix); ?>
 
-	<?php print render($title_prefix); ?>
-	<?php if ($block->subject): ?>
-		<h4<?php print $title_attributes; ?>><?php print $block->subject ?></h4>
-	<?php endif;?>
-	<?php print render($title_suffix); ?>
-
-	<div class="content"<?php print $content_attributes; ?>>
-		<?php print $content ?>
-	</div>
-
-</div>
+<?php print $content ?>
