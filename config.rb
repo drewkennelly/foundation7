ea1ee4c95361a38985632c1fe73253aa12f0ea16
Drew Kennelly <drew@with-033.local>
2013-11-02 12:14:19 +1100

    Upgrade to Foundation 4.3.2

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed
