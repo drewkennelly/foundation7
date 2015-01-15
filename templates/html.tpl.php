5e6fe1501cd6ceb850a6001d6e5854dfdd861d1e
Drew Kennelly <drew@withcollective.com>
2015-01-15 11:29:00 +1100

    Updating to Foundation 5.5.0

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -2,7 +2,7 @@
 <html class="no-js <?php print $classes; ?>" lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 <head>
   <?php echo $head; ?>
-  <meta name="viewport" content="width=device-width" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?php echo $head_title; ?></title>
   <?php echo $styles; ?>
   <?php echo $scripts; ?>
