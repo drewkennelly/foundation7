7246b9d9b16c507d8ece03c17cb8ba2fb99e56bc
Drew Kennelly <drew@withcollective.com>
2016-02-16 12:41:58 +1100

    foundation 6

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html class="no-js <?php print $classes; ?>" lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+<!doctype html>
+<html class="no-js <?php print $classes; ?>" lang="en">
 <head>
   <?php echo $head; ?>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?php echo $head_title; ?></title>
   <?php echo $styles; ?>
   <?php echo $scripts; ?>
-  <script>
-    $(document).foundation();
-  </script>
 </head>
 <body <?php print $attributes;?>>
   <?php print $page_top; ?>
